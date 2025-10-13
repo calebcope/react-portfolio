95f230ba94718ff975947af649ee18b893367921
ctcope228 <ctcope228@gmail.com>
2025-10-13 16:28:38 -0400

    assets

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,37 +1,43 @@
+import bloombase from '../assets/bloombase.png'
+import vingtun from '../assets/vingt-un.png'
+import awaken from '../assets/awaken.png'
+import blerkzoid from '../assets/blerkzoid.png'
+import react from '../assets/react-tailwind.png'
+
 export const projects = [
   {
     name: "BloomBase",
     description:
       "React Native mobile app integrating Firebase and plant APIs for planning, tracking, and managing farm workflows with real-time collaboration.",
-    image: "/assets/bloombase.png",
+    image: bloombase,
     link: "https://github.com/calebcope/bloom-base"
   },
   {
     name: "Vingt-Un",
     description:
       "Hybrid card game developed in Unity (C#) combining blackjack mechanics with roguelike progression, procedural randomness, and upgrade systems. Part of a greater project working with CSD Club members.",
-    image: "/assets/vingt-un.png",
+    image: vingtun,
     link: "https://github.com/Collaborative-Software-Development-Club/Fall-2025-Unity-Project"
   },
   {
     name: "Awaken",
     description:
       "Procedurally generated, roguelike dungeon crawler built in Unity (C#). Implemented a combination of random-walk and binary partitioning algorithms to ensure interesting, organic dungeons. Sprites and animations made in Aseprite. Built for CSD Summer Jam 2025, playable on itch.io.",
-    image: "/assets/awaken.png",
+    image: awaken,
     link: "https://jetjuice.itch.io/awaken"
   },
   {
     name: "3D Multiplayer Boss-Battler",
     description:
       "Cooperative multiplayer game using Unity Netcode with synchronized AI, animations, and real-time player combat in 3D environments. Designed, modeled and animated original characters in Blender. Built in collaboration with CSD Club.",
-    image: "/assets/blerkzoid.png",
+    image: blerkzoid,
     link: "https://github.com/Collaborative-Software-Development-Club/Spring-2025-Unity-Game"
   },
   {
     name: "This Website",
     description:
       "Built with React.js and Tailwind CSS. Fueled by my love of coffee.",
-    image: "/assets/react-tailwind.png",
+    image: react,
     link: "https://github.com/calebcope/react-portfolio"
   }
 ];
