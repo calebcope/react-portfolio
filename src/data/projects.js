eca03458a67619067e1967aab75baf8a39b159e4
ctcope228 <ctcope228@gmail.com>
2025-10-13 16:09:40 -0400

    assets

--- src/data/projects.js
+++ src/data/projects.js
@@ -3,35 +3,35 @@ export const projects = [
     name: "BloomBase",
     description:
       "React Native mobile app integrating Firebase and plant APIs for planning, tracking, and managing farm workflows with real-time collaboration.",
-    image: "/src/assets/bloombase.png",
+    image: "/assets/bloombase.png",
     link: "https://github.com/calebcope/bloom-base"
   },
   {
     name: "Vingt-Un",
     description:
       "Hybrid card game developed in Unity (C#) combining blackjack mechanics with roguelike progression, procedural randomness, and upgrade systems. Part of a greater project working with CSD Club members.",
-    image: "/src/assets/vingt-un.png",
+    image: "/assets/vingt-un.png",
     link: "https://github.com/Collaborative-Software-Development-Club/Fall-2025-Unity-Project"
   },
   {
     name: "Awaken",
     description:
       "Procedurally generated, roguelike dungeon crawler built in Unity (C#). Implemented a combination of random-walk and binary partitioning algorithms to ensure interesting, organic dungeons. Sprites and animations made in Aseprite. Built for CSD Summer Jam 2025, playable on itch.io.",
-    image: "/src/assets/awaken.png",
+    image: "/assets/awaken.png",
     link: "https://jetjuice.itch.io/awaken"
   },
   {
     name: "3D Multiplayer Boss-Battler",
     description:
       "Cooperative multiplayer game using Unity Netcode with synchronized AI, animations, and real-time player combat in 3D environments. Designed, modeled and animated original characters in Blender. Built in collaboration with CSD Club.",
-    image: "/src/assets/blerkzoid.png",
+    image: "/assets/blerkzoid.png",
     link: "https://github.com/Collaborative-Software-Development-Club/Spring-2025-Unity-Game"
   },
   {
     name: "This Website",
     description:
       "Built with React.js and Tailwind CSS. Fueled by my love of coffee.",
-    image: "/src/assets/react-tailwind.png",
+    image: "/assets/react-tailwind.png",
     link: "https://github.com/calebcope/react-portfolio"
   }
 ];
